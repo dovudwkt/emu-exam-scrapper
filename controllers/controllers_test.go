6df9692073557f695ee480452278076b55899cf6
Dovud Inomov <dinomov99@gmail.com>
2022-11-13 08:15:33 -0500

    test scrapExams function

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -1,27 +1,14 @@
 package controllers
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/dovudwkt/emu-exam-scrapper/entity"
 )
 
 func Test_scrapExams(t *testing.T) {
-	tests := []struct {
-		name string
-		want entity.Exams
-	}{
-		{
-			name: "test1",
-			want: []entity.Exam{},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := scrapExams(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("scrapExams() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	t.Run("test", func(t *testing.T) {
+		got := scrapExams()
+		if len(got) < 1 {
+			t.Errorf("scrapExams(): expecting more than 0 exams")
+		}
+	})
 }
